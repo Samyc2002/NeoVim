dcd4e3e074c61b332dd4dbebabbde76f75b1d343
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2024-10-11 22:18:53 +0530

    Some new git keybinds

--- lua/samy/git.lua
+++ lua/samy/git.lua
@@ -1,17 +1,31 @@
 local neogit = require('neogit')
+local gitsigns = require('gitsigns')
 
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>gs", function()
-	neogit.open({  })
+	neogit.open({})
 end)
+
 keymap.set("n", "<leader>gc", function()
 	neogit.open({ "commit" })
 end)
 keymap.set("n", "<leader>gp", function()
 	neogit.open({ "pull" })
 end)
+
 keymap.set("n", "<leader>gP", function()
 	neogit.open({ "push" })
 end)
 
+keymap.set("n", "<leader>ha", function()
+	gitsigns.stage_hunk()
+end)
+
+keymap.set("n", "<leader>lb", function()
+	gitsigns.blame_line()
+end)
+
+keymap.set("n", "<leader>hd", function()
+	gitsigns.diffthis()
+end)
